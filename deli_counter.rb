f988ae4ddd53d9fbcc775b88640cb9018b67ccf9
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-11 11:14:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line (katz_deli)
  if katz_deli.length > 1
    puts "The line is currently:"
    katz_deli.each_with_index do |name, i|
-     " #{i.to_i+1}. #{name}"
+    puts " #{i.to_i+1}. #{name}"
    end
  else
    puts "The line is currently empty."
