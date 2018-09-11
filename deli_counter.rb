a6b62e310078da675e51096fd062fbd4e2535e0e
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-11 17:20:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line (katz_deli)
    puts "The line is currently empty."
  else
    print "The line is currently:"
-   katz_deli.each_with_index do |name, index| 
+   katz_deli.each_with_index do |name, index|
      output << " #{index +1}. #{name}"}
    end
   puts output
