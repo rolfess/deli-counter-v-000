49cfa8d3ef94e9fc52da6ddb55278f7da83a5ad8
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-11 02:56:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+katz_deli = []
+
+def take_a_number (katz_deli, name)
+ counter = 1
+ puts "Welcome, #{name}. You are number #{counter} in line."
+end
+
+def line (katz_deli)
+ if katz_deli.length > 1 
+   #katz_deli.collect do |name|
+   #puts "The line is currently: #{katz_deli.}"
+  #end
+ else
+   puts "The line is currently empty."
+ end
+end
+
+def now_serving (katz_deli)
+
+end
+
