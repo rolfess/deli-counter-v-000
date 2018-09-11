bfc705f7e3517f4f328479c41c70d138a07a73b3
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-11 03:20:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,8 @@ end
 
 def line (katz_deli)
  if katz_deli.length > 1
-   katz_deli.each_with_index do |name,i|
-     puts "The line is currently: #{katz_deli.length}. #{katz_deli [katz_deli.length]}"
+   katz_deli.length.times do |i|
+     puts "The line is currently: #{i}. #{katz_deli [i]}"
    end
  else
    puts "The line is currently empty."
